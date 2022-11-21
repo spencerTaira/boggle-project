998eb2694d9bec9a4f600a6d01b5707be67b83ff
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-21 14:28:50 -0800

    Create join form

--- boggle/homepage/src/JoinForm.js
+++ boggle/homepage/src/JoinForm.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./Button.js";
 
-function JoinForm() {
+const defaultFormData = {
+  roomCode: "",
+}
+
+function JoinForm({ initialFormData=defaultFormData , joinRoom }) {
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+
+    setFormData(formData => ({
+      ...formData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    joinRoom(formData);
+    setFormData(initialFormData);
+  }
+
   return (
     <form className="JoinForm">
-      <p>Join Test</p>
+      <h3>Join Room</h3>
+      <div>
+        <label htmlFor="JoinForm-room-code">Room Code:</label>
+        <input
+          id="JoinForm-room-code"
+          className="form-control"
+          value={formData.roomCode}
+          onChange={handleChange}
+          name="room-code"
+          aria-label="Room Code"
+        />
+      </div>
+      <Button click={handleSubmit} label="Join Room" type="form-join"/>
     </form>
   );
 }
