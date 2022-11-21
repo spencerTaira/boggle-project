afab6b077f2d20c39ca1ad44f9d7ad48275d5a79
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-21 14:25:36 -0800

    Add handleChange and handleSubmit fns and Button

--- boggle/homepage/src/CreateForm.js
+++ boggle/homepage/src/CreateForm.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./Button.js";
 
-function CreateForm() {
+const defaultFormData = {
+  roomName: "",
+}
+
+function CreateForm({ initialFormData=defaultFormData , addRoom }) {
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+
+    setFormData(formData => ({
+      ...formData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    addRoom(formData);
+    setFormData(initialFormData);
+  }
+
   return (
     <form className="CreateForm">
-      <p>Create Test</p>
+      <h3>Create Room</h3>
+      <div>
+        <label htmlFor="CreateForm-room-name">Room Name:</label>
+        <input
+          id="CreateForm-room-name"
+          className="form-control"
+          value={formData.roomName}
+          onChange={handleChange}
+          name="room-name"
+          aria-label="Room Name"
+        />
+      </div>
+      <Button click={handleSubmit} label="Create Room" type="form-create"/>
     </form>
   );
 }
