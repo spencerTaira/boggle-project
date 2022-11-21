a39b44770c9d392418f9abd93881420384e99762
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-21 12:55:28 -0800

    Create states

---
+++ boggle/homepage/src/Homepage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Homepage() {
+  const [createOrJoin, setCreateOrJoin] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+  const [formResults, setFormResults] = useState({});
+
+  
+}
+
+export default Homepage;
