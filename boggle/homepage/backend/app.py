b9b5bf725884c5ce7771ac14240fc147352d3f48
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-23 11:22:53 -0800

    Change view fn name and add to addRoom view fn

--- boggle/homepage/backend/app.py
+++ boggle/homepage/backend/app.py
@@ -7,16 +7,18 @@ app.config["SECRET_KEY"] = "secret"
 
 # Retrieve rooms
 @app.get("/homepage")
-def homepage():
-    return {"rooms": 
-                [ {"name": "Room1", "id" : "1234"}, 
-                  {"name": "Room2", "id" : "567"}, 
+def displayRooms():
+    return {"rooms":
+                [ {"name": "Room1", "id" : "1234"},
+                  {"name": "Room2", "id" : "567"},
                   {"name": "Room3", "id" : "890"}]}
 
 # POST: add room
-@app.post("/homepage/<room_name>")
+@app.post("/<room_name>")
 def addRoom(room_name):
-    
+    room_name = "test"
+    return {}
+
 
 
 if __name__ == "__main__":
